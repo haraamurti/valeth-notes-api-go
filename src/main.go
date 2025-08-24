abdabf38e6dd0a0e619bc0131044a61b6c8ab356
haraamurti <haramurti@student.ub.ac.id>
2025-08-24 17:27:50 +0700

    second commit - static routing and html displaying

--- src/main.go
+++ src/main.go
@@ -2,23 +2,46 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+	"path"
 )
 
-func handlerIndex(w http.ResponseWriter, r *http.Request) {
-    var message = "Welcome"
-    w.Write([]byte(message))
+//handler welcome to localhost:9000
+func handlerwelcome (w http.ResponseWriter, r *http.Request) {
+	var filepath = path.Join("views", "index.html")
+	var tmpl, err = template.ParseFiles(filepath)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+    return
 }
 
+var data = map[string]interface{}{ //---> ini function buat ngaapin coba ada ?
+    "title": "vaelth notes api backend Golang Web",
+    "name":  "valeth",
+}
+
+err = tmpl.Execute(w, data)
+if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+	var message = "Its not you, Its us :("
+	w.Write([]byte(message))
+}
+}
+
+//handler for /hello
 func handlerHello(w http.ResponseWriter, r *http.Request) {
     var message = "Hello world!"
     w.Write([]byte(message))
 }
 
+//main function
 func main() {
-    http.HandleFunc("/", handlerIndex)
-    http.HandleFunc("/index", handlerIndex)
+    http.HandleFunc("/", handlerwelcome)
     http.HandleFunc("/hello", handlerHello)
+		http.Handle("/static/",
+        http.StripPrefix("/static/",
+            http.FileServer(http.Dir("assets"))))
 
     var address = "localhost:9000"
     fmt.Printf("server started at %s\n", address)
