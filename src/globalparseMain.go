b7923ad679ded868d7cacf79348d59412e968006
haraamurti <haramurti@student.ub.ac.id>
2025-08-24 23:09:35 +0700

    third commit (adding global parsing)

---
+++ src/globalparseMain.go
@@ -0,0 +1,35 @@
+/*package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+)
+
+type M map[string]interface{}
+func main() {
+    var tmpl, err = template.ParseGlob("views/*")
+    if err != nil {
+        panic(err.Error())
+        return
+    }
+
+	http.HandleFunc("/index", func(w http.ResponseWriter, r *http.Request) {
+    var data = M{"name": "Batman"}
+    err = tmpl.ExecuteTemplate(w, "index_new", data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+})
+
+http.HandleFunc("/about", func(w http.ResponseWriter, r *http.Request) {
+    var data = M{"name": "Batman"}
+    err = tmpl.ExecuteTemplate(w, "about", data)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
+})
+
+fmt.Println("server started at localhost:8080 .......")
+http.ListenAndServe("localhost:8080", nil)
+}*/
